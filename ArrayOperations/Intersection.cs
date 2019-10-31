a3b3e441e384683a5de665b537666cc283c05768
Satish Alturu <satish.alturu@elliemae.com>
2019-10-30 17:48:15 -0700

    Initial Version

---
+++ ArrayOperations/Intersection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    public class Intersection
+    {
+        /// <summary>
+        ///  Assumption : Arrays are in sorted order, otherwise this logic would not work
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <param name="list3"></param>
+        /// <returns></returns>
+        public static List<int>  GetInterSectionElements(int[] list1, int[] list2, int[] list3)
+        {
+            List<int> resultList = new List<int>();
+
+            int list1Index = 0, list2Index = 0, list3Index = 0;
+            
+            while(list1Index < list1.Length && list2Index < list2.Length && list3Index < list3.Length)
+            {
+                if (list1[list1Index] == list2[list2Index] && list2[list2Index] == list3[list3Index])
+                {
+                    resultList.Add(list1[list1Index]);
+                    list1Index++;
+                    list2Index++;
+                    list3Index++;
+                }
+                else if (list1[list1Index] < list2[list2Index])
+                    ++list1Index;
+                else if (list2[list2Index] < list3[list3Index])
+                    ++list2Index;
+                else
+                    ++list3Index;
+                                             
+            }
+
+            Console.WriteLine("Intersection elements: ");
+            foreach (int ele in resultList)
+                Console.Write(ele + "\t");
+            
+
+            return resultList;
+        }
+    }
+}
---
+++ ArrayOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intersection.GetInterSectionElements(new int[] { 2, 4, 7, 8 }, new int[] { 2, 4, 7, 9 }, new int[] { 2, 4, 7, 10 });
+        }
+    }
+}
